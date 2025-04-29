fdd2984fb452d42c9d198357140a5097a41340c0
gabrielmalheiross <gebrielmalheiros1515@gmail.com>
2025-04-29 17:04:34 -0400

    Primeiro commit

---
+++ app/src/main/java/com/example/notessqlite/activity/AddNoteActivity.kt
@@ -0,0 +1,15 @@
+package com.example.notessqlite.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.notessqlite.R
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
---
+++ app/src/main/java/com/example/notessqlite/activity/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.notessqlite.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.notessqlite.MainActivity
+import com.example.notessqlite.R
+import com.example.notessqlite.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+    }
+}
